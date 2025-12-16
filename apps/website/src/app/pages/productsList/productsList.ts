83ad742d0edf380fabd3e74fe191bac31e668a55
abdellatif4800 <mabdellatif82000@gmail.com>
2025-12-16 22:35:44 +0200

    update styles

--- apps/website/src/app/pages/productsList/productsList.ts
+++ apps/website/src/app/pages/productsList/productsList.ts
@@ -14,6 +14,7 @@ import { UpperCasePipe } from '@angular/common';
     RouterLink,
     FilterSidebar,
     ProductCard,
+    LucideAngularModule,
   ],
   templateUrl: './productsList.html',
   styleUrl: './productsList.css',
